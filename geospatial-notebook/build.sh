f2fc2edef556eb9c7beb8a8bea2a2c379097506e
mintspecter <mintspecter@gmail.com>
2022-05-24 16:33:26 +0900

    UPDATE: Using TempDir

--- geospatial-notebook/build.sh
+++ geospatial-notebook/build.sh
@@ -3,14 +3,15 @@
 echo 'JupyterLab for Geo-Spatial :: Build Image'
 
 source ./env.sh
+
 BASE_CONTAINER='docker.io/jupyter/pyspark-notebook:spark-3.2.1'
 
-docker build --rm \
+time_start=`date +%s` \
+&& podman build --rm \
     --tag $IMAGE \
     --build-arg BASE_CONTAINER=$BASE_CONTAINER \
     . \
-&& docker tag $IMAGE "$IMAGE_BASE:latest" \
-&& echo "Docker Push Command
-    $ docker push $IMAGE
-    & docker push $IMAGE_BASE:latest
-"
+&& time_end=`date +%s`
+time_total=$((time_end - time_start))
+
+echo "Build End - time: $time_total sec."
--- geospatial-notebook/init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source ./env.sh
-
-chown $NB_UID:$NB_GID ./work
